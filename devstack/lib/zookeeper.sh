3af2d627c8062f878ae679325b2343395bad76e6
Martin Chacon Piza <martin@chaconpiza.com>
2020-10-07 09:20:45 +0200

    change log settings

--- devstack/lib/zookeeper.sh
+++ devstack/lib/zookeeper.sh
@@ -60,8 +60,11 @@ function install_zookeeper {
     sudo useradd --system -g zookeeper zookeeper || true
     sudo tar -xzf ${zookeeper_tarball_dest} -C /opt
     sudo ln -sf /opt/zookeeper-${ZOOKEEPER_VERSION} /opt/zookeeper
-    sudo cp $PLUGIN_FILES/zookeeper/zoo.cfg $ZOOKEEPER_CONF_DIR
+    sudo cp $PLUGIN_FILES/zookeeper/* $ZOOKEEPER_CONF_DIR
     sudo chown -R zookeeper:zookeeper /opt/zookeeper/
+
+    sudo mkdir /var/log/zookeeper
+    sudo chown -R zookeeper:zookeeper /var/log/zookeeper
 
     sudo mkdir /var/lib/zookeeper
     sudo chown -R zookeeper:zookeeper /var/lib/zookeeper
